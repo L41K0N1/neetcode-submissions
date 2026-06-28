ab6150f9a2803bea7a2422d023f7475ff9468165
L41K0N1 <nechaev-kolya-95@mail.ru>
2026-06-28 19:11:02 +0500

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int windowLength = s1.length();
+
+        Map<Character, Integer> hashMap = new HashMap<>();
+        Map<Character, Integer> windowMap = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            hashMap.put(s1.charAt(i), hashMap.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < windowLength; i++) {
+            windowMap.put(s2.charAt(i), windowMap.getOrDefault(s2.charAt(i), 0) + 1);
+        }
+        
+        if (hashMap.equals(windowMap)) {
+            return true;
+        }
+
+        for (int i = 0; i + windowLength < s2.length(); i++) {
+
+            char leftChar = s2.charAt(i);
+            char rightChar = s2.charAt(i + windowLength);
+
+            int leftCharCount = windowMap.getOrDefault(leftChar, 0) - 1;
+            if (leftCharCount == 0) {
+                windowMap.remove(leftChar);
+            } else {
+                windowMap.put(leftChar, leftCharCount);
+            }
+
+            int rightCharCount = windowMap.getOrDefault(rightChar, 0) + 1;
+            windowMap.put(rightChar, rightCharCount);
+
+            if (hashMap.equals(windowMap)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
